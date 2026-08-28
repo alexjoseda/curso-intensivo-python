8c3d3f8954613d43a1bd11a9729a49c78ffebdfc
alex <limaalex.leal2019@gmail.com>
2026-08-27 22:34:59 -0300

    Adiciona exercícios de variáveis e strings

---
+++ Strings/Strings.py
@@ -0,0 +1,19 @@
+menssagen = "One of Python's strengths is its diverse community."
+print(menssagen)
+
+Menssagen = "One of Python's strengths is its diverse community."
+
+saldação = "ola, Alex gostaria de aprender python hoje?"
+print(saldação)
+
+Ana_leticia = "ola, ana leticia vamos aprender python hoje?"
+print(Ana_leticia.title())
+
+sitação = " 'O fracasso não encerra uma descoberta; ele mostra que ainda há algo a ser compreendido'"
+print(sitação)
+person_famous = "Tesla"
+print(sitação +" "+ person_famous)
+
+PERSON_FAMOUS = "NIKOLA\nTESLA"
+print(PERSON_FAMOUS)
+
---
+++ variaveis/variaveis.py
@@ -0,0 +1,8 @@
+mensagen = "HELLO WORLD"
+print(mensagen)
+
+Mensagen = "Hello Python Crash Course world!"
+print(Mensagen)
+
+texto = "Se você quiser encontrá os segredos do universo, pense em termos de energia, frequência e vibração."
+print(texto)
